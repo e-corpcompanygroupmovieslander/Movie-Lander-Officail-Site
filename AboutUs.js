b8d906d7d7ca05c2d5ed8c59cfd49a48e153a41e
e-corpcompanygroupmovieslander <127004003+e-corpcompanygroupmovieslander@users.noreply.github.com>
2023-03-10 07:52:25 +0300

    Update AboutUs.js
    Signed-off-by: e-corpcompanygroupmovieslander <127004003+e-corpcompanygroupmovieslander@users.noreply.github.com>

--- AboutUs.js
+++ AboutUs.js
@@ -1,3 +1,9 @@
-let AboutUsData='\
-<h1>About US</h1>\
-';
+let AboutUsData=`
+	<h3>About Movie Lander</h3>
+	<p>
+		&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp Movie Lander is a movie platform, that allows users to download movies directly to their devices and also watch directly on the application.
+		This application has a variety of movie types like animations, movies, series, kids, religious movies and others, thus giving the users the opportunity to choose from the various options.
+		<br>
+		&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp Movie Lander has ads that run on the first page on the app start, these ads have links to their respective webdites outside the appilcation. Movie Lander ads do not distrupt a user when using the application.
+	</p>
+`;
