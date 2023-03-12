f365312b90388a72094fedd68ac0b5b4ccc79e46
e-corpcompanygroupmovieslander <127004003+e-corpcompanygroupmovieslander@users.noreply.github.com>
2023-03-12 06:39:38 +0300

    Add files via upload
    Signed-off-by: e-corpcompanygroupmovieslander <127004003+e-corpcompanygroupmovieslander@users.noreply.github.com>

--- Translated.js
+++ Translated.js
@@ -1,3 +1,3 @@
 let TranslatedData='\
-<h1>Translated</h1>\
+<a href="https://drive.google.com/uc?export=download&id=1ls8LDVaJvZjnSMJQkiszDwcGQ1FP1VDl"><img loading="lazy" id="img" src="https://drive.google.com/uc?export=download&id=1mJo2A_trlw9ACi4OrKuEbzgCOoOjWXlN"></a>\
 ';
